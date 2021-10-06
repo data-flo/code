e8e1a2650e9874529f58270f5bcc242b1df2d37c
Khalil <ka10@sanger.ac.uk>
2021-10-06 16:21:19 +0100

    Update index.js

--- engine/adaptors/oracle-database/index.js
+++ engine/adaptors/oracle-database/index.js
@@ -1,7 +1,18 @@
 const knex = require("knex");
 
+// const path = require("path");
 // const oracledb = require("oracledb");
-// oracledb.initOracleClient({ libDir: "/usr/local/lib/instantclient_19_8" });
+// // On Windows and macOS, you can specify the directory containing the Oracle
+// // Client Libraries at runtime, or before Node.js starts.  On other platforms
+// // the system library search path must always be set before Node.js is started.
+// // See the node-oracledb installation documentation.
+// // If the search path is not correct, you will get a DPI-1047 error.
+// if (process.platform === "win32") { // Windows
+//   oracledb.initOracleClient({ libDir: "C:\\oracle\\instantclient_19_11" });
+// }
+// else if (process.platform === "darwin") { // macOS
+//   oracledb.initOracleClient({ libDir: path.join(process.env.HOME, "/Downloads/instantclient_19_8") });
+// }
 
 const validQuery = /^\s*SELECT/i;
 
