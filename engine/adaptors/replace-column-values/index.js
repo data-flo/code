0cbc39a9438c4cca58674288764aafaf1e080ffe
Khalil <ka10@sanger.ac.uk>
2021-10-20 16:41:09 +0100

    Update index.js

--- engine/adaptors/replace-column-values/index.js
+++ engine/adaptors/replace-column-values/index.js
@@ -11,7 +11,7 @@ module.exports = function (args) {
     args.target || args.source,
     (row) => {
       const sourceValue = row[args.source];
-      if (sourceValue) {
+      if (sourceValue || sourceValue === "") {
         if (typeof (sourceValue) === "number") {
           const targetValue = sourceValue.toString().replace(pattern, args.replacement);
           // convert back to a number if possible
