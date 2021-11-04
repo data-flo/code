0d3bf7533ca5d7dd63db720a26ee50cfe01835da
Khalil <ka10@sanger.ac.uk>
2021-11-04 15:38:17 +0000

    Update index.js

--- api/models/dataflow/index.js
+++ api/models/dataflow/index.js
@@ -9,4 +9,4 @@ schema.methods.cloneManifest = require("./clone-manifest");
 schema.statics.findByIdentifier = require("./find-by-identifier");
 schema.statics.getManifest = require("./get-manifest");
 
-module.exports = mongoose.model("Dataflow", schema);
+module.exports = mongoose.models.Dataflow || mongoose.model("Dataflow", schema);
