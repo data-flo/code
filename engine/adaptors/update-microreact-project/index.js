a5f8c5e3b2ef33b3fde8293602bf30b6fde0daf1
Khalil <ka10@sanger.ac.uk>
2021-11-04 15:39:03 +0000

    Update index.js

--- engine/adaptors/update-microreact-project/index.js
+++ engine/adaptors/update-microreact-project/index.js
@@ -1,43 +1,95 @@
 const createMicroreactDocument = require("microreact.js");
 
+function findFile(files, types) {
+  for (const fileId of Object.keys(files)) {
+    const file = files[fileId];
+    if (types.includes(file.format)) {
+      return fileId;
+    }
+  }
+  return undefined;
+}
+
 module.exports = async function createMicroreactProject(args, context) {
   let projectId = args.project;
-  if (args.project.length === 22) {
-    projectId = args.project;
-  }
-  else {
+
+  {
     const match = args.api.match(/.*\/project\/([123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ]{22})/i);
     if (match) {
       projectId = match[1];
     }
   }
 
-  const match = args.api.match(/(.*)\/api/i);
-
-  if (!match) {
-    throw new Error(`Invalid Microreact API URL: ${args.api}.`);
+  {
+    const match = args.api.match(/(.*)\/api/i);
+    if (!match) {
+      throw new Error(`Invalid Microreact API URL: ${args.api}.`);
+    }
   }
 
-  const request = await createMicroreactDocument({
+  const apiUrl = args.api.endsWith("/") ? args.api.substr(0, args.api.length - 1) : args.api;
+
+  const oldDocument = await context.request.postJson(
+    `${apiUrl}/projects/json?project=${projectId}`,
+    {},
+    { "access-token": args["access token"] },
+  );
+
+  const newDocument = await createMicroreactDocument({
     name: args.name,
     description: args.description,
-
     data: args.data,
     tree: args.tree,
     network: args.network,
-
-    settings: {
-      id: args["id column"],
-      timeline_field: args["timeline column"],
-      map_latitude: args["latitude column"],
-      map_longitude: args["longitude column"],
-    },
   });
 
+  if (args.data) {
+    const dataFileId = findFile(
+      oldDocument.files,
+      [
+        "text/csv",
+        "application/x-speadsheet",
+      ],
+    );
+    if (dataFileId) {
+      oldDocument.files[dataFileId] = newDocument.files["data-file-1"];
+    }
+  }
+
+  if (args.tree) {
+    const treeFileId = findFile(
+      oldDocument.files,
+      [
+        "text/x-nh",
+      ],
+    );
+    if (treeFileId) {
+      oldDocument.files[treeFileId] = newDocument.files["tree-file-1"];
+    }
+    else {
+      oldDocument.files["tree-file-1"] = newDocument.files["tree-file-1"];
+    }
+  }
+
+  if (args.network) {
+    const networkFileId = findFile(
+      oldDocument.files,
+      [
+        "text/vnd.graphviz",
+      ],
+    );
+    if (networkFileId) {
+      oldDocument.files[networkFileId] = newDocument.files["network-file-1"];
+    }
+    else {
+      oldDocument.files["network-file-1"] = newDocument.files["network-file-1"];
+    }
+  }
+
   const response = await context.request.postJson(
-    `${args.api}projects/update?project=${projectId}`,
-    request,
-    { "access-token": args["access token"] },
+    `${apiUrl}/projects/update?project=${projectId}`,
+    oldDocument,
+    { "access-token": args["access token"].trim() },
   );
 
   return {
