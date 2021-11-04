f0bc8c7fe56b2a7540060a72dff7210dc5aa4037
Khalil <ka10@sanger.ac.uk>
2021-11-04 15:38:14 +0000

    Update index.js

--- api/models/cache/index.js
+++ api/models/cache/index.js
@@ -2,4 +2,4 @@ const mongoose = require("mongoose");
 
 const schema = mongoose.Schema(require("./schema"));
 
-module.exports = mongoose.model("Cache", schema);
+module.exports = module.exports = mongoose.models.Cache || mongoose.model("Cache", schema);
