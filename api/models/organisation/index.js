9116550ddca88d8d75bf6e727bbaa89e624ea63e
Khalil <ka10@sanger.ac.uk>
2021-11-04 15:38:22 +0000

    Update index.js

--- api/models/organisation/index.js
+++ api/models/organisation/index.js
@@ -4,4 +4,4 @@ const schema = mongoose.Schema(require("./schema"));
 
 schema.pre("save", require("./pre-save"));
 
-module.exports = mongoose.model("Organisation", schema);
+module.exports = mongoose.models.Organisation || mongoose.model("Organisation", schema);
