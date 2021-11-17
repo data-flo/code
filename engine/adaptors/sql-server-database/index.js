1f9567fa478ff245071b552d003a0d72fd4fa6f4
Khalil <ka10@sanger.ac.uk>
2021-11-17 15:47:44 +0000

    Update index.js

--- engine/adaptors/sql-server-database/index.js
+++ engine/adaptors/sql-server-database/index.js
@@ -16,17 +16,21 @@ module.exports = async function (args) {
       },
     },
   });
+
   if (!validQuery.test(args.query)) {
     throw new Error("Invalid SQL Query: query should start with a SELECT statement.");
   }
+
   const results = await instance.raw(args.query);
   instance.destroy();
+
   const columns = new Set();
   for (const row of results) {
     for (const column of Object.keys(row)) {
       columns.add(column);
     }
   }
+
   return {
     data: {
       columns: Array.from(columns),
