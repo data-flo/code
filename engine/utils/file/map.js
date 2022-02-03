79973e2efdc8a305e587ea0fe3d79bf3a0d94d54
Khalil <ka10@sanger.ac.uk>
2022-02-03 16:32:58 +0000

    Update map.js

--- engine/utils/file/map.js
+++ engine/utils/file/map.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 
+const logger = require("cgps-application-server/logger");
+
 module.exports = function (fsMappings, filePath) {
   const normalisedFilePath = filePath.toLowerCase().replace(/\\/g, "/");
+  logger.debug("[utils.files.map]", { fsMappings, filePath, normalisedFilePath })
   for (const sourceMapping of Object.keys(fsMappings)) {
+    const targetMapping = fsMappings[sourceMapping];
+    logger.debug("\t", { normalisedFilePathStartsWithSourceMapping: normalisedFilePath.startsWith(sourceMapping.toLowerCase()), sourceMapping, targetMapping, sourceMappingLowerCase: sourceMapping.toLowerCase() })
     if (normalisedFilePath.startsWith(sourceMapping.toLowerCase())) {
-      const resolvedPath = path.resolve(fsMappings[sourceMapping], filePath.substr(sourceMapping.length).replace(/\\/g, "/"))
-      if (resolvedPath.startsWith(fsMappings[sourceMapping])) {
+      const resolvedPath = path.resolve(targetMapping, filePath.substr(sourceMapping.length).replace(/\\/g, "/"))
+      logger.debug("\t", { resolvedPath, resolvedPathStartsWithTargetMapping: resolvedPath.startsWith(targetMapping) })
+      if (resolvedPath.startsWith(targetMapping)) {
         return resolvedPath;
       }
     }
