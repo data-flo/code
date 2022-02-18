6558f9551fcb28c038c070826ed8ccb7d32be587
Anthony Underwood <anthony.underwood@cgps.group>
2022-02-18 18:35:03 +0000

    Force CSV file to be utf-8

--- engine/adaptors/datatable-to-csv-file/index.js
+++ engine/adaptors/datatable-to-csv-file/index.js
@@ -14,6 +14,9 @@ module.exports = function createDatatable(args) {
   );
 
   const file = new stream.Readable();
+  // Add universal BOM to force excel to read CSV in utf-8
+  // https://stackoverflow.com/questions/42462764/javascript-export-csv-encoding-utf-8-issue
+  file.push("\uFEFF", "utf8")
   file.push(csv, "utf8");
   file.push(null);
   file.name = args.filename;
